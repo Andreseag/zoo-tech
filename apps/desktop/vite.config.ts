f75316e611265ffc42a0dceae08b6f1186377a71
Edwin Castro <edwin.castro@agenciamatch.cl>
2025-10-30 11:48:44 -0500

    feat: setup to desktop app :sparkles:

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@zoo-tech/ui": path.resolve(__dirname, "../../packages/ui/src"),
+      "@zoo-tech/core": path.resolve(__dirname, "../../packages/core/src"),
+      "@zoo-tech/api": path.resolve(__dirname, "../../packages/api/src"),
+    },
+  },
+});
